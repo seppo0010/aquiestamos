87c443a62e52aac33c3de0a9cb50415a8296b9f8
Sebastian Waisbrot <seppo0010@gmail.com>
2017-02-11 20:20:50 -0300

    add check in button

--- shortcode.php
+++ shortcode.php
@@ -1,6 +1,12 @@
 <?php
 add_shortcode('ae-map', function($atts) {
-	return '<div id="ae_map" style="' . (!empty($atts['style']) ? $atts['style'] : '') . '"></div><script>aeSettings(' . json_encode(array(
+	wp_enqueue_style('ae-css', plugins_url('css/main.css', __FILE__), false, '1');
+	return
+	'<div id="ae_map_container" style="' . (!empty($atts['style']) ? $atts['style'] : '') . '">' .
+	'<div id="ae_map"></div>' .
+	'<div id="ae_checkin"><a href="javascript:void(null)">Aqu&iacute; estoy</a></div>' .
+	'</div>' .
+	'<script>aeSettings(' . json_encode(array(
 		'nonce' => wp_create_nonce('wp_rest'),
 		'base_url' => get_rest_url(),
 	)) . ');</script>';
