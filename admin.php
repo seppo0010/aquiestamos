f75a4b7917c16be721470bbbfd4c9e5408d8ee2a
Sebastian Waisbrot <seppo0010@gmail.com>
2017-02-16 07:36:50 -0300

    generalize admin form

--- admin.php
+++ admin.php
@@ -7,21 +7,37 @@ add_action('admin_init', function() {
 });
 
 add_action('admin_menu', function() {
-	add_options_page('Aqui Estamos Options', 'Aqui Estamos', 'manage_options', 'aqui-estamos', function() { ?>
+	add_options_page('Aqui Estamos Options', 'Aqui Estamos', 'manage_options', 'aqui-estamos', function() {
+	$settings = [
+		[
+			'label' => 'Google Maps key',
+			'name' => 'ae_google_maps_key',
+		],
+		[
+			'label' => 'Enable cache?<br />Only if you install a <a href="https://codex.wordpress.org/Class_Reference/WP_Object_Cache#Persistent_Cache_Plugins" target="_blank">cache plugin</a>',
+			'name' => 'ae_cache_enabled',
+			'type' => 'checkbox',
+		],
+	];
+	?>
 	<div>
 	<?php screen_icon(); ?>
-	<h2>My Plugin Page Title</h2>
+	<h2>Aqui estamos</h2>
 	<form method="post" action="options.php">
 	<?php settings_fields('ae_options'); ?>
 	<table>
+	<?php foreach ($settings as $setting) { ?>
 	<tr valign="top">
-	<th scope="row"><label for="ae_google_maps_key">Google Maps key</label></th>
-	<td><input type="text" id="ae_google_maps_key" name="ae_google_maps_key" value="<?php echo get_option('ae_google_maps_key'); ?>" /></td>
+	<th scope="row"><label for="<?php echo $setting['name']; ?>"><?php echo $setting['label']; ?></label></th>
+	<?php if (isset($setting['type']) && $setting['type'] === 'checkbox') { ?>
+	<td><input type="checkbox" id="<?php echo $setting['name']; ?>" name="<?php echo $setting['name']; ?>" value="1" <?php echo get_option($setting['name']) ? 'checked=""' : ''; ?> /></td>
+	<?php } elseif (isset($setting['type']) && $setting['type'] === 'textarea') { ?>
+	<td><textarea id="<?php echo $setting['name']; ?>" name="<?php echo $setting['name']; ?>"><?php echo htmlentities(get_option($setting['name']), ENT_QUOTES); ?></textarea></td>
+	<?php } else { ?>
+	<td><input type="text" id="<?php echo $setting['name']; ?>" name="<?php echo $setting['name']; ?>" value="<?php echo htmlentities(get_option($setting['name']), ENT_QUOTES); ?>" /></td>
+	<?php } ?>
 	</tr>
-	<tr valign="top">
-	<th scope="row"><label for="ae_cache_enabled">Enable cache? Only if you install a <a href="https://codex.wordpress.org/Class_Reference/WP_Object_Cache#Persistent_Cache_Plugins" target="_blank">cache plugin</a></label></th>
-	<td><input type="checkbox" id="ae_cache_enabled" name="ae_cache_enabled" value="1" <?php echo get_option('ae_cache_enabled') ? 'checked=""' : ''; ?> /></td>
-	</tr>
+	<?php } ?>
 	</table>
 	<?php submit_button(); ?>
 	</form>
