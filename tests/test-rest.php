955fcec54e68cf4293901a1685a782bd44269069
Sebastian Waisbrot <seppo0010@gmail.com>
2017-02-19 19:53:18 -0300

    add create item with content test

--- tests/test-rest.php
+++ tests/test-rest.php
@@ -51,7 +51,7 @@ class AERestTest extends WP_Test_REST_Controller_Testcase {
 		}
 	}
 
-	public function test_create_item() {
+	private function _test_create_items() {
 		$posts = array_reverse ( array_map (function( $user_id ) {
 			$data = $this->create_item( $user_id, true );
 			return [
@@ -66,6 +66,34 @@ class AERestTest extends WP_Test_REST_Controller_Testcase {
 			ae_get_posts_in_location([-90, 90], [-180, 180])['results'],
 			$posts
 		);
+	}
+
+	private function _test_create_item_with_content() {
+		wp_set_current_user( self::$users_id[0] );
+		$lat = rand(-9000, 9000) / 100;
+		$lon = rand(-18000, 18000) / 100;
+		$post_content = md5(rand());
+		$request = new WP_REST_Request( 'POST', '/ae/v1/checkin' );
+		$request->set_body_params( wp_parse_args( array(
+			'lat' => $lat,
+			'lon' => $lon,
+			'content' => $post_content,
+		) ) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 201 , $response->get_status() );
+		$this->assertContains(
+			[
+				'lat' => $lat,
+				'lng' => $lon,
+				'post_content' => $post_content,
+			],
+			ae_get_posts_in_location([-90, 90], [-180, 180])['results']
+		);
+	}
+
+	public function test_create_item() {
+		$this->_test_create_items();
+		$this->_test_create_item_with_content();
 	}
 
 	public function test_update_item() {}
