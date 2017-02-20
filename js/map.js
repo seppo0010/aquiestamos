6bd16be6b79e92f9d52f0401ae7f6e635bbaf027
Sebastian Waisbrot <seppo0010@gmail.com>
2017-02-20 06:05:49 -0300

    Do not checkin on refresh before login
    Fixes #11

--- js/map.js
+++ js/map.js
@@ -125,7 +125,9 @@
                         styles: settings.styles,
                     };
                     aeSetCheckinText(cookieData.checkinText);
-                    checkinAfterInit = true;
+                    if (settings.loggedin) {
+                        checkinAfterInit = true;
+                    }
                 } catch (e) {
                 }
                 document.cookie = cookieName + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/'
