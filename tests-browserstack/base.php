933e1be9b713f1be7537c24269cb490c38937a58
Sebastian Waisbrot <seppo0010@gmail.com>
2017-03-24 23:44:17 -0300

    Test close dialog
    Fixes #23

--- tests-browserstack/base.php
+++ tests-browserstack/base.php
@@ -5,11 +5,28 @@ use Facebook\WebDriver\WebDriverBy;
 
 abstract class BaseTest extends BrowserStackTest {
 	private $db;
+	private $options;
 
 	public function setUp() {
 		parent::setUp();
 		$this->db = mysql_connect('localhost', 'root', '');
 		mysql_select_db('testing', $this->db);
+		$this->options = array();
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+		foreach($this->options as $option => $value) {
+			$this->setOption($option, $value);
+		}
+	}
+
+	protected function escape($term) {
+		return mysql_real_escape_string($term, $this->db);
+	}
+
+	protected function row($query) {
+		return mysql_fetch_object($query);
 	}
 
 	protected function execute($sql) {
@@ -17,6 +34,7 @@ abstract class BaseTest extends BrowserStackTest {
 		if (!$query) {
 			$this->fail(mysql_error($this->db));
 		}
+		return $query;
 	}
 
 	protected function insertId() {
@@ -28,5 +46,14 @@ abstract class BaseTest extends BrowserStackTest {
 		self::$driver->findElement(WebDriverBy::id('user_login'))->sendKeys('aquiestamos');
 		self::$driver->findElement(WebDriverBy::id('user_pass'))->sendKeys('aquiestamos');
 		self::$driver->findElement(WebDriverBy::id('wp-submit'))->click();
+	}
+
+	protected function setOption($option, $newValue) {
+		$where = 'WHERE option_name = \'' . $this->escape($option) . '\'';
+		$query = $this->execute('SELECT option_value FROM wp_options ' . $where);
+		$oldValue = $this->row($query)->option_value;
+		$this->assertNotNull($oldValue);
+		$this->options[$option] = $oldValue;
+		$this->execute('UPDATE wp_options SET option_value = \'' . $this->escape($newValue) . '\' ' . $where);
 	}
 }
--- tests-browserstack/test-checkin.php
+++ tests-browserstack/test-checkin.php
@@ -45,4 +45,34 @@ class CheckinTest extends BaseTest {
 		self::$driver->wait()->until(WebDriverExpectedCondition::visibilityOf($thanks));
 		$this->assertNull(self::$driver->manage()->getCookieNamed('ae_checkin_location'));
 	}
+
+	public function testCloseCheckinDialog() {
+		$this->setOption('ae_checkin_text', '<a href="#" data-ae-close>Close</a>');
+		self::$driver->get("http://127.0.0.1:8000/");
+		$checkinDialog = self::$driver->findElement(WebDriverBy::id('ae_checkin'));
+		$this->assertTrue($checkinDialog->isDisplayed());
+		self::$driver->findElement(WebDriverBy::cssSelector('[data-ae-close]'))->click();
+		$this->assertFalse($checkinDialog->isDisplayed());
+	}
+
+	public function testCloseLoginDialog() {
+		$this->setOption('ae_login_text', '<a href="#" data-ae-close>Close</a>');
+		self::$driver->get("http://127.0.0.1:8000/");
+		self::$driver->findElement(WebDriverBy::cssSelector('[data-checkin]'))->click();
+		$loginDialog = self::$driver->findElement(WebDriverBy::id('ae_login'));
+		$this->assertTrue($loginDialog->isDisplayed());
+		self::$driver->findElement(WebDriverBy::cssSelector('[data-ae-close]'))->click();
+		$this->assertFalse($loginDialog->isDisplayed());
+	}
+
+	public function testCloseThanksDialog() {
+		$this->setOption('ae_thanks_text', '<a href="#" data-ae-close>Close</a>');
+		$this->login();
+		self::$driver->get("http://127.0.0.1:8000/");
+		self::$driver->findElement(WebDriverBy::cssSelector('[data-checkin]'))->click();
+		$thanksDialog = self::$driver->findElement(WebDriverBy::id('ae_thanks'));
+		$this->assertTrue($thanksDialog->isDisplayed());
+		self::$driver->findElement(WebDriverBy::cssSelector('[data-ae-close]'))->click();
+		$this->assertFalse($thanksDialog->isDisplayed());
+	}
 }
